36ba77daf70e1cf59b22d120caef16363267b3ff
sshakeel404 <sshakeel404@gmail.com>
2018-06-09 00:51:50 +0500

    Added Comments to the main functions

---
+++ GitTest/GitTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        /// <summary>
+        /// Application starts from here
+        /// </summary>
+        /// <param name="args">Any informtion about the application</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
